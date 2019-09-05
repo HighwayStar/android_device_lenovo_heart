1e598c0cafadd420d4cfe75f18cb709fc2d654ba
Bruno Martins <bgcngm@gmail.com>
2019-09-05 12:17:15 +0100

    heart: Define Soong namespaces in the common product makefile
    This is where it belongs.
    
    Change-Id: Ibea0e8fe5dbc26d8953a9735f0b38cf087805b76

--- device.mk
+++ device.mk
@@ -102,6 +102,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     KeyHandler
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
