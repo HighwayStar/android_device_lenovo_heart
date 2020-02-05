87a893a27786d514af16daa1f422c4f6738a292b
Vitaliy Tomin <highwaystar.ru@gmail.com>
2020-02-05 22:15:45 +0800

    heart: Update fingerprint from ZUI 11.5

--- lineage_heart.mk
+++ lineage_heart.mk
@@ -16,7 +16,7 @@ PRODUCT_MANUFACTURER := Lenovo
 PRODUCT_MODEL := Lenovo Z5 Pro GT
 PRODUCT_NAME := lineage_heart
 
-BUILD_FINGERPRINT := "Lenovo/heart/heart:9/PKQ1.190110.001/11.1.021_190802:user/release-keys"
+BUILD_FINGERPRINT := "Lenovo/heart/heart:10/QKQ1.191014.001/11.5.141_200118:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="heart-user 9 PKQ1.190110.001 11.1.021_190802 release-keys" \
